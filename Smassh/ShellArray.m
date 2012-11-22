b8ef30cda24db7c31c9b0b7f052da88c72ab58e5
perkee <hello@perk.ee>
2012-11-22 11:46:20 -0500

    Tried and got rid of a subclass of NSMutableArray, since that was a bad idea.

---
+++ Smassh/ShellArray.m
@@ -0,0 +1,13 @@
+//
+//  ShellArray.m
+//  Smassh
+//
+//  Created by perkee on 11/21/12.
+//  Copyright (c) 2012 perkee. All rights reserved.
+//
+
+#import "ShellArray.h"
+
+@implementation ShellArray
+
+@end
